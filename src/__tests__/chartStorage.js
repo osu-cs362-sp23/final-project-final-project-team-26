d006c55b7e642db22b9154f8a3cc1bbbb6245cf0
Parker Walters <parkerrwalters@gmail.com>
2023-06-01 17:39:35 -0700

    Test environment isn't working but ideas are present for execution

--- src/__tests__/chartStorage.js
+++ src/__tests__/chartStorage.js
@@ -5,6 +5,7 @@
 require("@testing-library/jest-dom/extend-expect")
 const domTesting = require("@testing-library/dom")
 const userEvent = require("@testing-library/user-event").default
+const chartStorage = require("../lib/chartStorage")
 
 const fs = require("fs")
 
@@ -17,3 +18,33 @@ function initDomFromFiles(htmlPath, jsPath) {
         require(jsPath)
     })
 }
+// function initDomFromFiles(jsPath) {
+//     jest.isolateModules(function () {
+//         require(jsPath)
+//     })
+// }
+
+test("Test that a chart is saved", function(){
+    initDomFromFiles(
+        "../src/index.html",
+        "../gallery/gallery.js"
+    )
+    // initDomFromFiles("../lib/chartStorage")
+    testChart = {
+        type: "line",
+        data: [
+            { x: 3, y: 2 },
+            { x: 1, y: 4 },
+        ],
+        xLabel: "x-axis",
+        yLabel: "y-axis",
+        title: "test chart",
+        color: "#ff4500"
+    }
+    testID = 1;
+    chartStorage.saveChart(testChart, testID)
+
+    const galleryTitle = domTesting.findByAltText(testChart.title)
+
+    expect(galleryTitle).toBe(testChart.title)
+})
