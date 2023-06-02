69bec31a55758d456f25829ea0281df00501bd4f
Parker Walters <parkerrwalters@gmail.com>
2023-06-01 17:01:19 -0700

    Install DOM environment necessities and started chartStorage test file

---
+++ src/__tests__/chartStorage.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+
+require("@testing-library/jest-dom/extend-expect")
+const domTesting = require("@testing-library/dom")
+const userEvent = require("@testing-library/user-event").default
+
+const fs = require("fs")
+
+function initDomFromFiles(htmlPath, jsPath) {
+    const html = fs.readFileSync(htmlPath, 'utf8')
+    document.open()
+    document.write(html)
+    document.close()
+    jest.isolateModules(function () {
+        require(jsPath)
+    })
+}
