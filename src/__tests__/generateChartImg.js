60a9d7eb89691e334c4a6c76ee51dbc98c160763
WaltzOT <123332116+WaltzOT@users.noreply.github.com>
2023-06-14 11:08:40 -0700

    All Unit Test Work(fixed generateCharImg)

--- src/__tests__/generateChartImg.js
+++ src/__tests__/generateChartImg.js
@@ -1,25 +1,21 @@
-/**
- * @jest-environment jsdom
- */
-
-global.fetch = require('jest-fetch-mock');
 
 require("whatwg-fetch")
-require("@testing-library/jest-dom/extend-expect")
-const domTesting = require("@testing-library/dom")
-const userEvent = require("@testing-library/user-event").default
 const generateChartImg = require("../lib/generateChartImg")
 
 const rest = require("msw").rest
 const setupServer = require("msw/node").setupServer
+const path = require("path")
 
-const fs = require("fs")
+const fs = require("node:fs")
+
+const pathToImage = path.join(__dirname, '../resources/dog.png')
+const imageData = fs.readFileSync(pathToImage)
+
 
 const server = setupServer(
     rest.post(
         "https://quickchart.io/chart",
         function (req, res, ctx) {
-            const imageData = fs.readFileSync("../resources/dog.png");
             return res(
             ctx.set('Content-Type', 'image/png'),
             ctx.body(imageData)
@@ -33,20 +29,19 @@ beforeAll(function () {
 })
 
 test("test generateChartImg function", async function () {
-
-    // type = "line";
-    // data = [
-    //         { x: 3, y: 2 },
-    //         { x: 1, y: 4 },
-    // ];
-    // xLabel ="x-axis";
-    // yLabel = "y-axis"; 
-    // title = "test chart";
-    // color = "#ff4500";
+    type = "line";
+    data = [
+            { x: 3, y: 2 },
+            { x: 1, y: 4 },
+    ];
+    xLabel ="x-axis";
+    yLabel = "y-axis"; 
+    title = "test chart";
+    color = "#ff4500";
     
-    // imgURL = generateChartImg(type, data, xLabel, yLabel, title, color);
+    imgURL = generateChartImg(type, data, xLabel, yLabel, title, color);
 
-    // expect(imgURL).toBeDefined();
+    expect(imgURL).toBeTruthy();
 });
 
 afterAll(function () {
