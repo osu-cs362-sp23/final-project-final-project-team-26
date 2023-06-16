b9204f2550f87717fcc878f5d475220e0a035d58
WaltzOT <123332116+WaltzOT@users.noreply.github.com>
2023-06-15 20:32:05 -0700

    reverting

--- cypress/e2e/generatingGraphs/scatterGraphGenertated.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Generate Scatter Graph', () => {
-    it('successfully generates a scatter graph', () => {
-      cy.visit('/') // change URL to match your dev URL
-      cy.contains('Scatter').click() //clicks the scatter graph button
-      cy.fillGraphData() //fills in graph, command can be found in commands.js
-      cy.get('#chart-img').should('be.visible') //checks that the graph image is visible
-    })
-  })
